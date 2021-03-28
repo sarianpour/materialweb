c0361d4cb8b457e8dda902883b25103984bd1da6
sarianpour <samanarianpour@gmail.com>
2021-03-28 18:50:52 +0430

    starter web fixed

---
+++ lib/layout/adaptive.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:adaptive_breakpoints/adaptive_breakpoints.dart';
+
+/// Returns a boolean value whether the window is considered medium or large size.
+///
+/// Used to build adaptive and responsive layouts.
+bool isDisplayDesktop(BuildContext context) =>
+    getWindowType(context) >= AdaptiveWindowType.medium;
+
+/// Returns boolean value whether the window is considered medium size.
+///
+/// Used to build adaptive and responsive layouts.
+bool isDisplaySmallDesktop(BuildContext context) {
+  return getWindowType(context) == AdaptiveWindowType.medium;
+}
