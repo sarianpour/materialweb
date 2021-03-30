a565948b130aa7c7822c596ca0e7a9fa910655ea
sarianpour <samanarianpour@gmail.com>
2021-03-30 18:35:40 +0430

    style fixed

--- lib/widgets/raisedButton.dart
+++ lib/widgets/raisedButton.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:materialweb/widgets/plain_text.dart';
 
-class WidgetRaisedButton extends StatelessWidget {
+class WidgetRaisedButton extends StatefulWidget {
   WidgetRaisedButton({this.onPressed, this.text});
   final Function onPressed;
   final String text;
 
   @override
+  _WidgetRaisedButtonState createState() => _WidgetRaisedButtonState();
+}
+
+class _WidgetRaisedButtonState extends State<WidgetRaisedButton> {
+  @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
     return RaisedButton(
         padding: EdgeInsets.symmetric(vertical: 25, horizontal: 50),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(100),
-        ),
-        //color: secondaryColor,
-        onPressed: onPressed,
-        child: PlainText(text: text));
+            borderRadius: BorderRadius.circular(5),
+            side: BorderSide(color: colorScheme.onSecondary, width: 2)),
+//        hoverColor: Colors.transparent,
+        disabledTextColor: Colors.red,
+//textColor: Colors.green,
+        //color: Colors.transparent,
+        onPressed: widget.onPressed,
+        child: PlainText(text: widget.text));
   }
 }
+//PlainText(text: widget.text)
+//RaisedButton(
+//padding: EdgeInsets.symmetric(vertical: 25, horizontal: 50),
+//shape: RoundedRectangleBorder(
+//borderRadius: BorderRadius.circular(5),
+//side: BorderSide(color: colorScheme.onSecondary, width: 2)),
+//hoverColor: colorScheme.onSecondary,
+//disabledTextColor: Colors.red,
+////textColor: Colors.green,
+//color: Colors.transparent,
+//onPressed: widget.onPressed,
+//child: Text(widget.text))
